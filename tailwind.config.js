0c49076c9962f2c16d6c252cecdde02e9f2f2ec1
Xavier <xaviertuominen@gmail.com>
2022-07-11 20:51:31 -0500

    primary

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: ["./**/*.html"],
   theme: {
     screens: {
-      'sm': {'min': '640px', 'max': '767px'},
+      'sm': {'min': '300', 'max': '767px'},
       // => @media (min-width: 640px and max-width: 767px) { ... }
 
       'md': {'min': '768px', 'max': '1023px'},
